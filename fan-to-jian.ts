ce5e1ddf785afc40e55c5bfe44d274b1cc161941
Freeman Chen <pucelle@163.com>
2022-06-07 22:29:53 +0800

    修订翻译.

--- fan-to-jian.ts
+++ fan-to-jian.ts
@@ -3,12 +3,23 @@ import * as fs from 'fs-extra'
 import * as path from 'path'
 
 
-async function run () {
+const fromDir = path.resolve(__dirname, 'Translations_TODO/Chinese')
+
+
+/** 将繁体替换为简体. */
+async function jianToFan() {
+	let fileNames = await fs.readdir(fromDir)
+
+	for (let fileName of fileNames) {
+		await replaceOneFile(path.resolve(fromDir, fileName))
+	}
+}
+
+async function replaceOneFile(filePath: string) {
 	const converter = new OpenCC('t2s.json')
-	let filePath = path.resolve(__dirname, 'Translations/Chinese/Chinese_NpcText.sql')
 	let text = (await fs.readFile(filePath)).toString('utf8')
 	let converted = await converter.convertPromise(text)
 	await fs.writeFile(filePath, converted)
 }
 
-run()
+jianToFan()
